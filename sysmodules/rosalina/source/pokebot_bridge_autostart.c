7cbfb2e389588881afb0750f9515622851c25d73
SHGliscor <evo4986@gmail.com>
2026-08-18 13:48:45 +0100

    Remove constructor-based Pokebot bridge autostart

--- sysmodules/rosalina/source/pokebot_bridge_autostart.c
+++
@@ -1,8 +0,0 @@
-/* Start the Pokebot read-only bridge from Rosalina's existing constructor phase. */
-#include "pokebot_bridge.h"
-
-__attribute__((constructor))
-static void PokebotBridge_AutoStart(void)
-{
-    PokebotBridge_CreateThread();
-}
